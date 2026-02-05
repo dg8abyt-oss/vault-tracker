eb4951f60c550c964e02b929b021d54891da95c0
dg8abyt-oss <dg8abyt@gmail.com>
2026-02-06 01:11:02 +0530

    LIOX Update

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ status: "Online", time: new Date().toISOString() });
+  res.status(200).json({ status: "Online", v: "6.0" });
 }
